766f82a7e581a70019b30afcad040e9e03f8263c
bobby2lopez <bobbysworld05@gmail.com>
2022-05-17 10:32:13 -0700

    bullet expl

--- FinalProject/Assets/bullet.cs
+++ FinalProject/Assets/bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+    public GameObject hitEffect;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,14 @@ public class bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
         
     }
 }
