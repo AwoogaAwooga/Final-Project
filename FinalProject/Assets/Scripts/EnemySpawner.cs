6bc6b1ebdf2e666972a0bcb2d84cbd105d65b8e5
bobby2lopez <bobbysworld05@gmail.com>
2022-05-17 10:55:12 -0700

    spawner no anim just pic

--- FinalProject/Assets/Scripts/EnemySpawner.cs
+++ FinalProject/Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject Zombie;
+    [SerializeField]
+    private float zombInterval = 3.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(spawnEnemy(zombInterval, Zombie));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
     }
 }
